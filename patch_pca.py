afcaec38cad6f352146227b34a0154e58307666a
kevtool <kevinlim9171@gmail.com>
2025-08-28 06:14:12 -0700

    pixelwise subtraction

--- patch_pca.py
+++ patch_pca.py
@@ -1,8 +1,14 @@
 import numpy as np
 
-def patch_pca(frame_t, frame_tn1):
+def patch_pca(frame_t, frame_tn1, topleft, patch_size=32, entire_frame=False):
+
+    # extract roi
+    x0, y0 = topleft
+    roi_t = frame_t[y0:y0+patch_size, x0:x0+patch_size]
+    roi_tn1 = frame_tn1[y0:y0+patch_size, x0:x0+patch_size]
+
     # color pca
-    data = np.concatenate((frame_t, frame_tn1), axis=0)
+    data = np.concatenate((roi_t, roi_tn1), axis=0)
     data = data.reshape(-1, 3).T
     mean_color = np.mean(data, axis=1, keepdims=True)
 
@@ -20,6 +26,14 @@ def patch_pca(frame_t, frame_tn1):
     eigenvector = eigenvectors[0, :]
 
     # pixelwise subtraction
-    pca_t = frame_t.T @ eigenvector
-    pca_tn1 = frame_tn1.T @ eigenvector
-    diff = pca_t - pca_tn1
+    if entire_frame:
+        print(frame_t.shape, eigenvector.shape)
+        pca_t = frame_t @ eigenvector
+        pca_tn1 = frame_tn1 @ eigenvector
+        diff = pca_t - pca_tn1
+
+    else:
+        print(roi_t.shape, eigenvector.shape)
+        pca_t = roi_t @ eigenvector
+        pca_tn1 = roi_tn1 @ eigenvector
+        diff = pca_t - pca_tn1
